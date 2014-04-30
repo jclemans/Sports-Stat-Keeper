065dbb3b66cc6892c88137009366ad61971ff9ce
Jeremy Clemans & Tara Scherner de la Fuente <student@epicodus.com>
2014-04-30 12:25:54 -0700

    add team model and controller

--- js/app.js
+++ js/app.js
@@ -1,11 +1,54 @@
-App = Ember.Application.create();
+//APP CREATE
+window.App = Ember.Application.create();
 
-App.Router.map(function() {
-  // put your routes here
+App.ApplicationAdapter = DS.LSAdapter.extend({
+  namespace: 'app-emberjs'
 });
 
-App.IndexRoute = Ember.Route.extend({
+//ROUTES
+App.Router.map(function() {
+  this.resource('teams', { path: '/' });
+});
+
+App.TeamsRoute = Ember.Route.extend({
   model: function() {
-    return ['red', 'yellow', 'blue'];
+    return this.store.find('team');
+  }
+});
+
+//MODELS
+App.Team = DS.Model.extend({
+  teamName: DS.attr('string')
+});
+
+//FIXTURES
+App.Team.FIXTURES = [
+  {
+    id: 1,
+    teamName: "Screaming Banshees"
+  },
+
+  {
+    id: 2,
+    teamName: "Shrieking Eels"
+  }
+];
+
+//CONTROLLERS
+
+App.TeamsController = Ember.ArrayController.extend({
+  actions: {
+    createTeam: function() {
+      var teamName = this.get('newName');
+      if (!teamName.trim()) { return; }
+
+      var team = this.store.createRecord('team', {
+        teamName: teamName
+      });
+
+      this.set('newName', '');
+
+      team.save();
+    }
   }
 });
