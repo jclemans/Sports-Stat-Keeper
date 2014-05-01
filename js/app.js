e60d86cf187095daea3f71d557dc38549da34c7d
Jeremy Clemans & Tara Scherner de la Fuente <student@epicodus.com>
2014-04-30 17:16:17 -0700

    add team handlebars and nested players

--- js/app.js
+++ js/app.js
@@ -1,13 +1,24 @@
 //APP CREATE
 window.App = Ember.Application.create();
 
+App.ApplicationSerializer = DS.LSSerializer.extend();
 App.ApplicationAdapter = DS.LSAdapter.extend({
   namespace: 'app-emberjs'
 });
 
 //ROUTES
 App.Router.map(function() {
-  this.resource('teams', { path: '/' });
+  this.resource('teams', { path: '/' }, function() {
+    this.resource('team', { path: '/teams/:team_id' }, function() {
+      this.resource('players', { path: '/teams/:team_id/players' });
+    });
+  });
+});
+
+App.PlayersRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.find('player');
+  }
 });
 
 App.TeamsRoute = Ember.Route.extend({
@@ -16,38 +27,60 @@ App.TeamsRoute = Ember.Route.extend({
   }
 });
 
+App.TeamRoute = Ember.Route.extend({
+  model: function(params) {
+    var team = this.store.find('team', params.team_id);
+    return team;
+  }
+});
+
 //MODELS
 App.Team = DS.Model.extend({
+  players: DS.hasMany('player'),
   teamName: DS.attr('string')
 });
 
-//FIXTURES
-App.Team.FIXTURES = [
-  {
-    id: 1,
-    teamName: "Screaming Banshees"
-  },
+App.Player = DS.Model.extend({
+  team: DS.belongsTo('team'),
+  playerName: DS.attr('string')
+});
 
-  {
-    id: 2,
-    teamName: "Shrieking Eels"
-  }
-];
 
 //CONTROLLERS
+
+App.PlayersController = Ember.ArrayController.extend({
+  actions: {
+    createPlayer: function() {
+      var playerName = this.get('newName');
+      var player = this.store.createRecord('player', {
+        playerName: playerName
+      });
+
+      this.set('newName', '');
+      player.save();
+    }
+  }
+});
 
 App.TeamsController = Ember.ArrayController.extend({
   actions: {
     createTeam: function() {
       var teamName = this.get('newName');
-      if (!teamName.trim()) { return; }
-
       var team = this.store.createRecord('team', {
         teamName: teamName
       });
 
       this.set('newName', '');
+      team.save();
+    }
+  }
+});
 
+App.TeamController = Ember.ObjectController.extend({
+  actions: {
+    removeTeam: function() {
+      var team = this.get('model');
+      team.deleteRecord();
       team.save();
     }
   }
